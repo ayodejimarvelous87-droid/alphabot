fc1b7ee1d27451855e588f3df652c7843d1e38d6
Ayodeji <ayodwjimarvelous87@gmail.com>
2026-07-10 22:56:25 +0100

    Fix product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,21 +8,16 @@ const {
   deleteProduct
 } = require("../controllers/productController");
 
-
 // Add product
 router.post("/add", addProduct);
-
 
 // Get products
 router.get("/", getProducts);
 
-
 // Update product
 router.put("/:id", updateProduct);
 
-
 // Delete product
 router.delete("/:id", deleteProduct);
-
 
 module.exports = router;
