81bf2eb85bff634e379b63d0e2985091fd025314
Ayodeji <ayodwjimarvelous87@gmail.com>
2026-07-23 11:11:38 +0100

    Force IPv4 for Gmail SMTP

--- services/emailService.js
+++ services/emailService.js
@@ -1,3 +1,4 @@
+const dns = require("dns");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
@@ -5,12 +6,19 @@ host:"smtp.gmail.com",
 port:587,
 secure:false,
 requireTLS:true,
+family:4,
 auth:{
 user:process.env.EMAIL_USER,
 pass:process.env.EMAIL_PASS
 },
 connectionTimeout:10000,
-socketTimeout:10000
+socketTimeout:10000,
+tls:{
+rejectUnauthorized:false
+},
+dnsLookup:(hostname, options, callback)=>{
+dns.lookup(hostname,{family:4},callback);
+}
 });
 
 const sendEmail = async(to, subject, text)=>{
@@ -24,11 +32,11 @@ subject,
 text
 });
 
-console.log("Email sent successfully to:", to);
+console.log("Email sent successfully to:",to);
 
 }catch(error){
 
-console.log("EMAIL ERROR:", error.message);
+console.log("EMAIL ERROR:",error.message);
 throw error;
 
 }
